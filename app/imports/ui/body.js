5eab45d63595f6e77c4df74e714c12ee44123025
ribvid <ribvid@gmail.com>
2017-01-01 18:58:19 +0100

    Prijava / registracija

--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -1,3 +1,41 @@
 import { Template } from 'meteor/templating';
  
 import './body.html';
+
+if (Meteor.isClient) {
+	Template.register.events({
+		'submit form': function(event) {
+			event.preventDefault();
+			var usernameVar = event.target.registerUsername.value;
+			var passwordVar = event.target.registerPassword.value;
+			var emailVar = event.target.registerEmail.value;
+			var bioVar = event.target.registerBio.value;
+			var imageVar = event.target.registerImage.value;
+
+			Accounts.createUser({
+				username: usernameVar,
+				password: passwordVar,
+				email: emailVar,
+				bio: bioVar,
+				profileImage: imageVar
+			});
+		}
+	});
+
+	Template.login.events({
+		'submit form': function(event) {
+			event.preventDefault();
+			var usernameVar = event.target.loginUsername.value;
+			var passwordVar = event.target.loginPassword.value;
+
+			Meteor.loginWithPassword(usernameVar, passwordVar);
+		}
+	});
+
+	Template.dashboard.events({
+		'click .logout': function(event) {
+			event.preventDefault();
+			Meteor.logout();
+		}
+	});
+}
